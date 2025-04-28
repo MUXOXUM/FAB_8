04bf557211b1919cea0aea5227f27ccb1d0d7750
Alex <aizvirtovic@gmail.com>
2025-04-28 05:41:57 +0300

    fix)

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-const PORT = 1482;
+const PORT = 1480;
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.get('*', (req, res) => {
